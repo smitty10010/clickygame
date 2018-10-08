09e4c68a67a376496f3a623a354a580e20e922ff
Robert <35673753+smitty10010@users.noreply.github.com>
2018-10-08 00:39:20 -0600

    Updates

---
+++ precache-manifest.564ecf0aeeacaf99a7b9e52e683fa450.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "b1316ec2d24c2c301cfa",
+    "url": "/clickygame/static/js/main.b1316ec2.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clickygame/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "ef82f0cb3e20c2188962b4230cf4af9f",
+    "url": "/clickygame/index.html"
+  }
+];
